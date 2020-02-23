562e7fc9ab332ef090602d0dd27c1c9f6d02fa8a
Jalal Al Attar <jalalalattar89@gmail.com>
2020-02-23 11:38:49 +0100

    HW-Week1-Jalal

--- week1/homeWork/HackYourTemperature-I/server.js
+++ week1/homeWork/HackYourTemperature-I/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars');
 const axios = require('axios');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get('/',(req, res) => res.send('Hello from backend to frontend!'))
-app.listen(port, ()=> console.log(` HackYourTemperature I ${port}`))
+app.get('/',(req, res) => res.send('Hello from backend to frontend!'));
+app.listen(port, ()=> console.log(` HackYourTemperature I ${port}`));
