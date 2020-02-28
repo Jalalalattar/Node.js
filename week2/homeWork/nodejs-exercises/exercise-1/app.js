eda375b22e7b8edef367b953312a8d1ed21b35e3
Jalal Al Attar <jalalalattar89@gmail.com>
2020-02-28 22:36:09 +0100

    Hw week2 exercise 1

---
+++ week2/homeWork/nodejs-exercises/exercise-1/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const fs = require("fs");
+const port = 3000;
+
+app.use(express.json());
+
+// CREATE
+app.post('/blogs', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  fs.writeFileSync(title, content);
+  res.end('ok')
+})
+
+// UPDATE
+app.put('/blogs', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end('it is updated')
+  }
+  else {
+    res.end('post does not exist');
+  }
+})
+
+// DELETE
+app.delete('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  fs.unlinkSync(title);
+  res.end('it is deleted');
+})
+
+// READ
+app.get('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  res.sendfile(title);
+  res.end('blabla');
+}) 
+
+app.listen(port, ()=> console.log(`Connect OK ${port}`));
